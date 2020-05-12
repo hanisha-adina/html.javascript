af3cdf6fcf3240e7b92c9b7d870c2ccba70d4e76
hanisha-adina <64548354+hanisha-adina@users.noreply.github.com>
2020-05-12 19:44:12 +0530

    Create innerhtml.js

---
+++ innerhtml.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<body>
+<h2>my first web page</h2>
+<p>my first paragraph.</p>
+<p id="demo"></p>
+<script>
+document.getElementById("demo").innerHTML=5+6;
+</script>
+</body>
+</html>
